e7a57f835ac863e0469f0d009eeefe31b50f679d
blackblackout483 <blackblackout483@gmail.com>
2024-12-21 23:07:19 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,13 @@
+immutable_var = 5, 1, True, 'String', 7, [1, 2]
+print(immutable_var)
+
+immutable_var[0] = 3
+print(immutable_var)
+
+immutable_list = [1, 0, 5, 3]
+immutable_list[1] = 5
+print(immutable_list)
+
+
+
+
